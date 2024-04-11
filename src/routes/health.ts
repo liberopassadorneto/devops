aba27c09811085fed66f730e036cbdded73fa3d3
Libero Passador Neto <libero.neto@vitta.me>
2024-04-11 15:03:02 -0300

    register health route

--- src/routes/health.ts
+++ src/routes/health.ts
@@ -4,7 +4,7 @@ import { ZodTypeProvider } from "fastify-type-provider-zod";
 export const health = async (app: FastifyInstance) => {
   app
   .withTypeProvider<ZodTypeProvider>()
-  .get('/health', async(request,reply)=>{
-    return reply.status(200).send()
+  .get('/healthz', async(request, reply) => {
+    return reply.status(200).send("OK!")
   })
 }
